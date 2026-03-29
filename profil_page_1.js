2f2a0f12a9ca40bb6096cb79320c0075e0aa55c0
km8smpitiqra-droid <km8smpitiqra@gmail.com>
2026-03-29 22:13:02 +0700

    Add files via upload

--- profil_page_1.js
+++ profil_page_1.js
@@ -1,21 +1,21 @@
-PhoneResponsive={"785":{
-"pageLayer":{"w":785,"h":515,"bgColor":"#ffffff"}
+TabletResponsive={"1009":{
+"pageLayer":{"w":1009,"h":662,"bgColor":"#ffffff"}
 ,
-"button41738":{"x":714,"y":13,"w":45.000000,"h":45.000000,"stylemods":[{"sel":"div.button41738Text","decl":" { position:fixed; left:4px; top:2px; width:36px; height:40px;}"},{"sel":"span.button41738Text","decl":" { display:table-cell; position:relative; width:36px; height:40px; vertical-align:middle; text-align:center; line-height:11px; font-size:11px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC"  ,"fd": "images/home.png" ,"fdO": "images/home.png" ,"fdD": "images/home.png" ,"fdDi": "images/home.png" ,"p": "M 11.000000 0.000000 L 33.000000 0.000000 L 35.250000 0.250000 L 37.312500 0.875000 L 40.812500 3.250000 L 43.125000 6.750000 L 43.750000 8.812500 L 44.000000 11.000000 L 44.000000 33.000000 L 43.750000 35.250000 L 43.125000 37.312500 L 40.812500 40.812500 L 37.312500 43.125000 L 35.250000 43.750000 L 33.000000 44.000000 L 11.000000 44.000000 L 8.875000 43.812500 L 6.812500 43.187500 L 4.937500 42.187500 L 3.250000 40.812500 L 1.875000 39.125000 L 0.875000 37.250000 L 0.187500 35.187500 L 0.000000 33.000000 L 0.000000 11.000000 L 0.250000 8.812500 L 0.875000 6.750000 L 3.250000 3.250000 L 6.750000 0.875000 L 8.812500 0.250000 L 11.000000 0.000000 z"}
+"button41738":{"x":918,"y":17,"w":57.000000,"h":57.000000,"stylemods":[{"sel":"div.button41738Text","decl":" { position:fixed; left:5px; top:2px; width:46px; height:52px;}"},{"sel":"span.button41738Text","decl":" { display:table-cell; position:relative; width:46px; height:52px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADkAAAA5CAYAAACMGIOFAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAkSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAAAAAAAA4NQAMv0AAR9x/jcAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADkAAAA5CAYAAACMGIOFAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAkSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAAAAAAAA4NQAMv0AAR9x/jcAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADkAAAA5CAYAAACMGIOFAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAkSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAAAAAAAA4NQAMv0AAR9x/jcAAAAASUVORK5CYII=" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADkAAAA5CAYAAACMGIOFAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAkSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAAAAAAAA4NQAMv0AAR9x/jcAAAAASUVORK5CYII="  ,"fd": "images/home.png" ,"fdO": "images/home.png" ,"fdD": "images/home.png" ,"fdDi": "images/home.png" ,"p": "M 14.000000 0.000000 L 42.000000 0.000000 L 44.812500 0.312500 L 47.437500 1.125000 L 49.812500 2.375000 L 51.937500 4.125000 L 53.625000 6.187500 L 54.875000 8.562500 L 55.687500 11.187500 L 56.000000 14.000000 L 56.000000 42.000000 L 55.687500 44.812500 L 54.875000 47.437500 L 53.625000 49.812500 L 51.937500 51.937500 L 49.812500 53.625000 L 47.437500 54.875000 L 44.812500 55.687500 L 42.000000 56.000000 L 14.000000 56.000000 L 11.250000 55.750000 L 8.687500 54.937500 L 6.250000 53.687500 L 4.125000 51.937500 L 2.375000 49.812500 L 1.062500 47.375000 L 0.250000 44.750000 L 0.000000 42.000000 L 0.000000 14.000000 L 0.312500 11.187500 L 1.125000 8.562500 L 2.375000 6.187500 L 4.125000 4.125000 L 6.187500 2.375000 L 8.562500 1.125000 L 11.187500 0.312500 L 14.000000 0.000000 z"}
 ,
-"image40703":{"x":0,"y":0,"w":785,"h":515,"bOffBottom":0,"i":"images/presentasi_kimia_materi_senyawa_benzena_dalam_gaya_ungu_ilustratif_24_.png"}
+"image40703":{"x":0,"y":0,"w":1009,"h":662,"bOffBottom":0,"i":"images/presentasi_kimia_materi_senyawa_benzena_dalam_gaya_ungu_ilustratif_24_.png"}
 ,
-"text40941":{"x":253,"y":285,"w":94,"h":35,"txtscale":100,"bOffBottom":0}
+"text40941":{"x":325,"y":366,"w":121,"h":45,"txtscale":100,"bOffBottom":0}
 ,
-"text40893":{"x":252,"y":194,"w":89,"h":35,"txtscale":100,"bOffBottom":0}
+"text40893":{"x":324,"y":249,"w":114,"h":45,"txtscale":100,"bOffBottom":0}
 ,
-"text40899":{"x":253,"y":241,"w":90,"h":37,"txtscale":100,"bOffBottom":0}
+"text40899":{"x":325,"y":310,"w":116,"h":47,"txtscale":100,"bOffBottom":0}
 ,
-"text40783":{"x":174,"y":282,"w":70,"h":50,"txtscale":100,"bOffBottom":0}
+"text40783":{"x":224,"y":362,"w":90,"h":55,"txtscale":100,"bOffBottom":0}
 ,
-"text40740":{"x":172,"y":237,"w":90,"h":30,"txtscale":100,"bOffBottom":0}
+"text40740":{"x":221,"y":305,"w":116,"h":39,"txtscale":100,"bOffBottom":0}
 ,
-"text40716":{"x":171,"y":190,"w":87,"h":32,"txtscale":100,"bOffBottom":0}
+"text40716":{"x":220,"y":244,"w":112,"h":41,"txtscale":100,"bOffBottom":0}
 ,
 "RCDResetQuestion":function(){
 try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
@@ -24,24 +24,24 @@ try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)
 "RCDResultResize":function(){}
 ,"preload":[]
 },
-"480":{
-"pageLayer":{"w":480,"h":763,"bgColor":"#ffffff"}
+"785":{
+"pageLayer":{"w":785,"h":1000,"bgColor":"#ffffff"}
 ,
-"button41738":{"x":437,"y":17,"w":28.000000,"h":28.000000,"stylemods":[{"sel":"div.button41738Text","decl":" { position:fixed; left:3px; top:2px; width:21px; height:23px;}"},{"sel":"span.button41738Text","decl":" { display:table-cell; position:relative; width:21px; height:23px; vertical-align:middle; text-align:center; line-height:7px; font-size:7px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF+gAAAAfgMMXAABWzVarQAAAABJRU5ErkJggg==" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF+gAAAAfgMMXAABWzVarQAAAABJRU5ErkJggg==" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF+gAAAAfgMMXAABWzVarQAAAABJRU5ErkJggg==" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF+gAAAAfgMMXAABWzVarQAAAABJRU5ErkJggg=="  ,"fd": "images/home.png" ,"fdO": "images/home.png" ,"fdD": "images/home.png" ,"fdDi": "images/home.png" ,"p": "M 6.000000 0.000000 L 21.000000 0.000000 L 23.312500 0.500000 L 25.250000 1.750000 L 26.500000 3.687500 L 27.000000 6.000000 L 27.000000 21.000000 L 26.500000 23.312500 L 25.250000 25.250000 L 23.312500 26.500000 L 21.000000 27.000000 L 6.000000 27.000000 L 3.750000 26.562500 L 1.812500 25.250000 L 0.500000 23.312500 L 0.000000 21.000000 L 0.000000 6.000000 L 0.500000 3.687500 L 1.750000 1.750000 L 3.687500 0.500000 L 6.000000 0.000000 z"}
+"button41738":{"x":714,"y":17,"w":45.000000,"h":45.000000,"stylemods":[{"sel":"div.button41738Text","decl":" { position:fixed; left:4px; top:2px; width:36px; height:40px;}"},{"sel":"span.button41738Text","decl":" { display:table-cell; position:relative; width:36px; height:40px; vertical-align:middle; text-align:center; line-height:11px; font-size:11px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAfSURBVGhD7cExAQAAAMKg9U9tDQ+gAAAAAAAAAIA7Ax/RAAGoeI1KAAAAAElFTkSuQmCC"  ,"fd": "images/home.png" ,"fdO": "images/home.png" ,"fdD": "images/home.png" ,"fdDi": "images/home.png" ,"p": "M 11.000000 0.000000 L 33.000000 0.000000 L 35.250000 0.250000 L 37.312500 0.875000 L 40.812500 3.250000 L 43.125000 6.750000 L 43.750000 8.812500 L 44.000000 11.000000 L 44.000000 33.000000 L 43.750000 35.250000 L 43.125000 37.312500 L 40.812500 40.812500 L 37.312500 43.125000 L 35.250000 43.750000 L 33.000000 44.000000 L 11.000000 44.000000 L 8.875000 43.812500 L 6.812500 43.187500 L 4.937500 42.187500 L 3.250000 40.812500 L 1.875000 39.125000 L 0.875000 37.250000 L 0.187500 35.187500 L 0.000000 33.000000 L 0.000000 11.000000 L 0.250000 8.812500 L 0.875000 6.750000 L 3.250000 3.250000 L 6.750000 0.875000 L 8.812500 0.250000 L 11.000000 0.000000 z"}
 ,
-"image40703":{"x":0,"y":0,"w":480,"h":315,"bOffBottom":0,"i":"images/presentasi_kimia_materi_senyawa_benzena_dalam_gaya_ungu_ilustratif_24_.png"}
+"image40703":{"x":0,"y":0,"w":785,"h":515,"bOffBottom":0,"i":"images/presentasi_kimia_materi_senyawa_benzena_dalam_gaya_ungu_ilustratif_24_.png"}
 ,
-"text40941":{"x":155,"y":366,"w":58,"h":21,"txtscale":100,"bOffBottom":0}
+"text40941":{"x":253,"y":366,"w":94,"h":35,"txtscale":100,"bOffBottom":0}
 ,
-"text40893":{"x":154,"y":249,"w":54,"h":21,"txtscale":100,"bOffBottom":0}
+"text40893":{"x":252,"y":249,"w":89,"h":35,"txtscale":100,"bOffBottom":0}
 ,
-"text40899":{"x":155,"y":310,"w":55,"h":22,"txtscale":100,"bOffBottom":0}
+"text40899":{"x":253,"y":310,"w":90,"h":37,"txtscale":100,"bOffBottom":0}
 ,
-"text40783":{"x":107,"y":362,"w":43,"h":50,"txtscale":100,"bOffBottom":0}
+"text40783":{"x":174,"y":362,"w":70,"h":50,"txtscale":100,"bOffBottom":0}
 ,
-"text40740":{"x":105,"y":305,"w":55,"h":50,"txtscale":100,"bOffBottom":0}
+"text40740":{"x":172,"y":305,"w":90,"h":30,"txtscale":100,"bOffBottom":0}
 ,
-"text40716":{"x":105,"y":244,"w":53,"h":50,"txtscale":100,"bOffBottom":0}
+"text40716":{"x":171,"y":244,"w":87,"h":32,"txtscale":100,"bOffBottom":0}
 ,
 "RCDResetQuestion":function(){
 try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
